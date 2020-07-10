ab4dd24298a31e038c1ccd19856c563853d4cc37
JeremyT313 <xdetroit@live.com>
2020-07-10 13:23:41 -0400

    added javascript and css

---
+++ main.js
@@ -0,0 +1,14 @@
+let player1_score = 0;
+let player2_score = 0;
+const winning_score = 3;
+
+// while (player1_score < winning_score && player2_score < winning_score) {
+//   choice = ['rock', 'paper', 'scissors'];
+//   computer = choice[Math.floor(Math.random() * 3)];
+//   player1 =
+// }
+
+choice = ['rock', 'paper', 'scissors'];
+computer = choice;
+console.log(computer[Math.floor(Math.random() * 3)]);
+// console.log(Math.floor(Math.random() * 3));
